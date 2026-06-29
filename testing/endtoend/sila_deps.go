2b0802b8037bb0bd1d6559eac4b4804f0c3eec6f
Ahmed Ibrahim <ahmed@sila.local>
2026-06-29 12:32:51 +0300

    deps: rename execution blank import to Sila

--- testing/endtoend/sila_deps.go
+++ testing/endtoend/sila_deps.go
@@ -9,10 +9,10 @@ import (
 	_ "github.com/sila-chain/Sila/cmd/utils"         // Required for Sila e2e.
 	_ "github.com/sila-chain/Sila/common"            // Required for Sila e2e.
 	_ "github.com/sila-chain/Sila/console"           // Required for Sila e2e.
-	_ "github.com/sila-chain/Sila/eth"               // Required for Sila e2e.
 	_ "github.com/sila-chain/Sila/log"               // Required for Sila e2e.
 	_ "github.com/sila-chain/Sila/metrics"           // Required for Sila e2e.
 	_ "github.com/sila-chain/Sila/node"              // Required for Sila e2e.
+	_ "github.com/sila-chain/Sila/sila"              // Required for Sila e2e.
 	_ "github.com/sila-chain/Sila/sila/downloader"   // Required for Sila e2e.
 	_ "github.com/sila-chain/Sila/silaclient"        // Required for Sila e2e.
 )
